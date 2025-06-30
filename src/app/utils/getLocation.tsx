6c64d18b33bd0b9c80cb3bb71264aa31dba94438
sanjeev <sanjeev.b@myclnq.co.in>
2025-06-30 15:33:43 +0530

    fix location

--- src/app/utils/getLocation.tsx
+++ src/app/utils/getLocation.tsx
@@ -5,12 +5,7 @@ export const getCurrentLocation = (): Promise<GeolocationPosition> =>
     } else {
       navigator.geolocation.getCurrentPosition(
         resolve,
-        reject,
-        {
-          enableHighAccuracy: true, // Ask for best accuracy
-          timeout: 10000,           // Wait up to 10 seconds
-          maximumAge: 0             // Don't use cached location
-        }
+        reject
       );
     }
   });
